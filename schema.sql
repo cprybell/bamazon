d585b6b3a462d28e3905223cc50cc3a6c2125473
cprybell <cprybell@sbcglobal.net>
2019-11-13 20:55:55 -0800

    Added data to the schema.sql file

--- schema.sql
+++ schema.sql
@@ -15,3 +15,10 @@ CREATE TABLE products (
   stock_quantity INTEGER NOT NULL,
   PRIMARY KEY (item_id)
 );
+
+USE bamazon;
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Apples", "Produce", 1.05, 25), ("Coca Cola", "Beverages", 2.54, 12), ("White Claws", "Alcohol", 17.33, 1),
+("Light Bulbs", "Home Improvement", 10.21, 9), ("Ground Beef", "Butcher", 8.09, 33), ("Old Spice Body Wash", "Bathroom", 3.01, 12),
+("Bananas", "Produce", 0.10, 104), ("Nugo Bars", "Sports Bars", 1.25, 17), ("Jack Daniels", "Alcohol", 46.73, 23),
+("Pepsi", "Beverages", 2.21, 21);
