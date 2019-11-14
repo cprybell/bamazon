7647807fec2a9e8c766bee4ddc04954a9f0dca03
cprybell <cprybell@sbcglobal.net>
2019-11-13 21:07:18 -0800

    Added files to json.pacakge. Added to code so when program is initially run it prints out the products in the database

--- bamazon.js
+++ bamazon.js
@@ -1,3 +1,31 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    user: "root",
+  
+    password: "a-6Intruder",
+    database: "bamazon"
+  });
+
+var query = connection.query(
+    "SELECT * FROM products",
+    function(err, res) {
+        if (err) throw err;
+        readElements(res);
+});
+
+
+function readElements(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log("ID :" + arr[i].item_id + " | Product Name: " + arr[i].product_name + " | Department: " + arr[i].department_name + " | Price: " 
+        + arr[i].price + " | Stock Quantity: " + arr[i].stock_quantity );
+    }
+    console.log("-----------------------------------");
+    connection.end();
+}
+
